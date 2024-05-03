2734622c4bcf0a66be514d613de19391b303fce8
momentum24 <mohamedalim559@gmail.com>
2024-05-03 04:00:06 +0300

    add ISR

--- ICU_1/Debug/ICU_1_main.d
+++ ICU_1/Debug/ICU_1_main.d
@@ -1,10 +1,19 @@
 ICU_1_main.o ICU_1_main.o: ../ICU_1_main.c ../STD_TYPES.h ../BIT_MATH.h \
-  ../PORT_interface.h ../CLCD_interface.h
+  ../PORT_interface.h ../TIMER_interface.h ../EXTI_interface.h \
+  ../DIO_interface.h ../GIE_interface.h ../CLCD_interface.h
 
 ../STD_TYPES.h:
 
 ../BIT_MATH.h:
 
 ../PORT_interface.h:
+
+../TIMER_interface.h:
+
+../EXTI_interface.h:
+
+../DIO_interface.h:
+
+../GIE_interface.h:
 
 ../CLCD_interface.h:
--- ICU_1/ICU_1_main.c
+++ ICU_1/ICU_1_main.c
@@ -9,55 +9,50 @@
 #include "BIT_MATH.h"
 
 #include "PORT_interface.h"
-//#include "TIMER_interface.h"
-//#include "EXTI_interface.h"
-//#include "DIO_interface.h"
-//#include "GIE_interface.h"
+#include "TIMER_interface.h"
+#include "EXTI_interface.h"
+#include "DIO_interface.h"
+#include "GIE_interface.h"
 #include "CLCD_interface.h"
-#include <util/delay.h>
 
 void Response(void);
 
 void main()
 {
-//	TIMER0_voidInit();
-//	EXTI_u8SetCallBack(&Response);
-//	EXTI_u8IntEnable(INT0);
-//	TIMER1_voidInit();
+	PORT_voidInIt();
+	EXTI_u8SetCallBack(&Response);
+	EXTI_u8IntEnable(INT0);
+	TIMER1_voidInit();
 	CLCD_voidInit();
-//	GIE_voidEnable();
-
+	TIMER0_voidInit();
+	GIE_voidEnable();
 
 	while(1)
 	{
-		CLCD_voidGoToXY(0,0);
-		CLCD_voidSendString("OCR1A value=");
 	}
 }
 
-//void Response(void)
-//{
-//	static u16 cnt = 0, Period_Time , On_Time;
-//	if(cnt == 0)
-//	{
-//		TIMER1_voidSetPreload(0);
-//		cnt++;
-//	}
-//	else if(cnt == 1)
-//	{
-//		Period_Time = TIMER1_u16GetTimerCounterValue();
-//		EXTI_u8Int0SetSenseCotrol(FALLING_EDGE);
-//		cnt++;
-//	}
-//	else if(cnt == 2)
-//	{
-//		On_Time = (TIMER1_u16GetTimerCounterValue() - Period_Time);
-//		cnt++;
-//		EXTI_u8IntDisable(INT0);
-//		CLCD_voidSendString("Period Time=");
-//		CLCD_voidDisplayNumber(Period_Time);
-//		CLCD_voidGoToXY(1,0);
-//		CLCD_voidSendString("On Time=");
-//		CLCD_voidDisplayNumber(On_Time);
-//	}
-//}
+void Response(void)
+{
+	static u16 cnt = 0, Period_Time , On_Time;
+	if(cnt == 0)
+	{
+		TIMER1_voidSetPreload(0);
+	}
+	else if(cnt == 1)
+	{
+		EXTI_u8Int0SetSenseCotrol(FALLING_EDGE);
+		Period_Time = TIMER1_u16GetTimerCounterValue();
+	}
+	else if(cnt == 2)
+	{
+		On_Time = (TIMER1_u16GetTimerCounterValue() );
+		EXTI_u8IntDisable(INT0);
+		CLCD_voidSendString("Period Time=");
+		CLCD_voidDisplayNumber(Period_Time);
+		CLCD_voidGoToXY(1,0);
+		CLCD_voidSendString("On Time=");
+		CLCD_voidDisplayNumber(On_Time);
+	}
+	cnt++;
+}
