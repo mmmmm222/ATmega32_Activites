2e8eb32de8dc111effcc93642496684a012ccb8d
momentum24 <mohamedalim559@gmail.com>
2024-05-04 22:46:34 +0300

    updeate

---
+++ WDT/Debug/WDT_main.d
@@ -0,0 +1,10 @@
+WDT_main.o WDT_main.o: ../WDT_main.c ../STD_TYPES.h ../BIT_MATH.h \
+  ../PORT_interface.h ../DIO_interface.h
+
+../STD_TYPES.h:
+
+../BIT_MATH.h:
+
+../PORT_interface.h:
+
+../DIO_interface.h:
