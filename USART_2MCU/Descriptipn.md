f6305600bb6d2f8cd5a712b039545b3c18cdc714
momentum24 <mohamedalim559@gmail.com>
2024-05-05 07:51:07 +0300

    communicate between two MCUs through uart

---
+++ USART_2MCU/Descriptipn.md
@@ -0,0 +1,2 @@
+Using USART protocol to communicate between two mcu when MCU1 send '1' mcu2 will turn on the led when mcu1 send '2' mcu2 will turn off the led
+
---
+++ USART_2MCU/MCU_2/Debug/MCU_2_main.d
@@ -0,0 +1,10 @@
+MCU_2_main.o MCU_2_main.o: ../MCU_2_main.c ../STD_TYPES.h \
+  ../PORT_interface.h ../USART_interface.h ../DIO_interface.h
+
+../STD_TYPES.h:
+
+../PORT_interface.h:
+
+../USART_interface.h:
+
+../DIO_interface.h:
